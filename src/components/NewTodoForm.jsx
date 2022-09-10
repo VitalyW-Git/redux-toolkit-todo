6d974c8c8e86546772fa75f16f9720adfc2456b0
VitalyW-Git <>
2022-09-10 20:05:18 +0300

    start

--- src/components/NewTodoForm.jsx
+++ src/components/NewTodoForm.jsx
@@ -2,7 +2,7 @@ const NewTodoForm = ({ value, updateText, handleAction }) => {
   return (
     <label>
       <input
-        placeholer='new todo'
+        placeholder='new todo'
         value={value}
         onChange={(e) => updateText(e.target.value)}
       />
